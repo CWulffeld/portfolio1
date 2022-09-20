584d0ff46f064360e5a97d73d160fcf9cf812c10
LauraLSJN <lsjn@ruc.dk>
2022-09-20 11:32:17 +0200

    Tilføjelse af variabler og metoder

--- src/com/company/Tankers.java
+++ src/com/company/Tankers.java
@@ -2,8 +2,9 @@ package com.company;
 
 public class Tankers extends Vessel{
 
-    int compartments = 10;
-    int capacity = 5;
+    //int compartments = 10;
+    int nowCapacity;
+    int maxCapacity = 10;
     int volume[] = new int[9];
 
     @Override
