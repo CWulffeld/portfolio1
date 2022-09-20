c2a059552b0edcb4a553e0b5a487e75d4cc6a18e
LauraLSJN <lsjn@ruc.dk>
2022-09-20 11:40:50 +0200

    Tilføjelse af variabler og metoder

--- src/com/company/containerVessel.java
+++ src/com/company/containerVessel.java
@@ -9,7 +9,16 @@ public class containerVessel extends Vessel{
     int containerLength;
 
 
+public containerVessel(String flagNation, int length, int width, int nowContainer){
+    super.flagNation= flagNation;
+    super.length = length;
+    super.width = width;
+    this.nowContainer = nowContainer;
+    this.maxContainer = 10;
+    this.containerLength = 10;
+    this.containerWidth = 15;
 
+}
 
     //The method loadingCargo() is used to set relevant information for vessels
     // and make the cargo amount accessible through methods.
@@ -27,6 +36,8 @@ public class containerVessel extends Vessel{
     @Override
     public void loadingCargo() {
         int containerAmount = this.maxContainer - this.nowContainer;
+        System.out.println("Amount of container on the container vessel: " + this.nowContainer);
+        System.out.println("Amount of container on the container vessel space left: " + containerAmount);
         System.out.println(containerAmount);
     }
 
