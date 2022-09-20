86fbcb5e843262ccd3bd117ddf57a638f54c0e34
LauraLSJN <lsjn@ruc.dk>
2022-09-20 11:32:17 +0200

    Tilføjelse af variabler og metoder

--- src/com/company/RoroVessel.java
+++ src/com/company/RoroVessel.java
@@ -2,6 +2,12 @@ package com.company;
 
 public class RoroVessel extends Vessel {
 
+    int totalAmountLaneMeters;
+    int car;
+    int truck;
+    int carLength = 8;
+    int truckLength = 30;
+
     @Override
 public void loadingCargo(){
 
--- src/com/company/containerVessel.java
+++ src/com/company/containerVessel.java
@@ -2,15 +2,38 @@ package com.company;
 
 public class containerVessel extends Vessel{
 
-    private int cargo = 10; //Kan indeholde specifikt antal af cargo (Måske det skal være protected)
+    int cargo; //Kan indeholde specifikt antal af cargo (Måske det skal være protected)
     int nowContainer;
     int maxContainer;
 
 
 
+    //The method loadingCargo() is used to set relevant information for vessels
+    // and make the cargo amount accessible through methods.
+   /* @Override
+    public void loadingCargo(String flagNation, int length, int width, int nowContainer, int maxContainer) {
+        super.flagNation= flagNation;
+        super.length = length;
+        super.width = width;
+        this.nowContainer = nowContainer;
+        this.maxContainer = maxContainer;
+    }*/
+
+
 
     @Override
     public void loadingCargo() {
+        int containerAmount = this.maxContainer - this.nowContainer;
+        System.out.println(containerAmount);
+    }
+
+
+
+    //should compute the fraction of the total capacity that is used.
+    // For tankers a compartment is assumed to be utilized if it is not empty since it then cannot be used for other products.
+    @Override
+    public void utilityLevelOfCapacity(){
 
     }
+
 }
